752165a1cf18bc258790fa855ea90017d5107686
RomanDolid <dolid.rv@gmail.com>
2021-01-25 17:37:53 +0200

    add

--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,16 +1,21 @@
 import axios from 'axios';
 import newsRender from './newsRender';
 
-const fetchNews = {
+const apiKey = 'dfe51acaeb93423681f9593a203ef679';
+const options = {
+  headers: {
+    Authorization: apiKey,
+  },
+};
+ const fetchNews = {
   baseUrl: 'http://newsapi.org/v2/everything',
   query: '',
   pageSize: 10,
   page: 1,
-  apiKey: 'dfe51acaeb93423681f9593a203ef679',
 
   fetchNewses: async (query, page) => {
     const { data } = await axios.get(
-      `${fetchNews.baseUrl}?q=${query}&pageSize=${fetchNews.pageSize}&page=${page}&apiKey=${fetchNews.apiKey}`,
+      `${fetchNews.baseUrl}?q=${query}&pageSize=${fetchNews.pageSize}&page=${page}`, options
     );
 
     return newsRender.getNewsArray(data);
